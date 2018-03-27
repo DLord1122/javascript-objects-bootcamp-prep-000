268b0f12a002a82038ccb6ab21b1360c040544d3
Dennis Lord <lorddennis1006@gmail.com>
2018-03-27 12:01:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = {artist: 'songTitle'}
 function updatePlaylist(playlist, artistName, songTitle) {playlist[artistName] = songTitle
 playlist}
-function removeFromPlaylist(playlist, artistName){return playlist}
+function removeFromPlaylist(playlist, artistName){delete playlist[artistName]return playlist}
